1a221f24cfe59fe96a5c83fc48a7e919c128182e
qinwei <qinwei@gp.com>
2020-01-02 10:37:39 +0800

    fix login and add logout

--- src/main/java/life/qinwei/community/controller/AuthorizeController.java
+++ src/main/java/life/qinwei/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.qinwei.community.dto.GithubUser;
 import life.qinwei.community.mapper.UserMapper;
 import life.qinwei.community.model.User;
 import life.qinwei.community.provider.GithubProvider;
+import life.qinwei.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -31,6 +33,9 @@ public class AuthorizeController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -49,10 +54,8 @@ public class AuthorizeController {
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
             user.setAvatarUrl(githubUser.getAvatar_url());
-            userMapper.insert(user);
+            userService.createOrUpdate(user);
             respose.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
@@ -60,4 +63,15 @@ public class AuthorizeController {
             return "redirect:/";
         }
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/life/qinwei/community/mapper/UserMapper.java
+++ src/main/java/life/qinwei/community/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package life.qinwei.community.mapper;
 
 import life.qinwei.community.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
@@ -17,4 +14,10 @@ public interface UserMapper {
 
     @Select("select * from USER where id = #{id}")
     User findById(@Param("id")Integer id);
+
+    @Select("select * from USER where account_id = #{accountId}")
+    User finByAccountId(@Param("accountId")String accountId);
+
+    @Update("update user set name = #{name}, token = #{token}, gmt_modified = #{gmtModified}, avatar_url = #{avatarUrl}, id = #{id}")
+    void update(User user);
 }
